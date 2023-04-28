e20cb1d65325b72d60c1f87010eb2c9e2f308c4b
dvimukthi <deshanvimukthi59@gmail.com>
2023-04-28 16:09:44 +0530

    signup connect

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 const express = require("express");
 const mysql = require("mysql");
+const cors = require("cors");
 
-var app = express();
+const app = express();
 
 const hostname = "localhost";
 const port = 3001;
+
+app.use(express.json());
+app.use(cors());
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "app_user",
+  password: "12345",
+  database: "orrp_ai",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to Database!");
+});
+
+// app.get("/", function (req, res) {
+//   res.send(JSON.stringify({ status: "OK" }));
+// });
+
+app.post("/signup", (req, res) => {
+  const name = req.body.name;
+  const password = req.body.password;
+  connection.query(
+    "INSERT INTO user (name, password) VALUES(?, ?)",
+    [name, password],
+    (err, result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.send({ message: "Enter Correct Details" });
+      }
+    }
+  );
+});
 
 var server = app.listen(port, hostname, () => {
   console.log(`Server Started on : http://${hostname}:${port}/`);
---
+++ src/services/data_services.js
@@ -0,0 +1,13 @@
+// var mysql = require("mysql");
+// // var User = require("../model/User");
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "app_user",
+//   password: "12345",
+//   database: "orrp_ai",
+// });
+
+// connection.connect(function (err) {
+//   if (err) throw err;
+// });
