f60b2ae8a794f46669d80bf47645dcaffdfe9dbe
dvimukthi <deshanvimukthi59@gmail.com>
2023-04-29 23:56:54 +0530

    index

--- src/model/User.js
+++ src/model/User.js
@@ -1,42 +1,42 @@
-// class User {
-//   connection(id, name, email, password) {
-//     this.id = id;
-//     this.name = name;
-//     // this.email = email;
-//     this.password = password;
-//   }
+class User {
+  connection(id, name, email, password) {
+    this.id = id;
+    this.name = name;
+    // this.email = email;
+    this.password = password;
+  }
 
-//   getId() {
-//     return this.id;
-//   }
+  getId() {
+    return this.id;
+  }
 
-//   setId(id) {
-//     this.id = id;
-//   }
+  setId(id) {
+    this.id = id;
+  }
 
-//   getName() {
-//     return this.name;
-//   }
+  getName() {
+    return this.name;
+  }
 
-//   setName(name) {
-//     this.name = name;
-//   }
+  setName(name) {
+    this.name = name;
+  }
 
-//   // getEmail() {
-//   //   return this.email;
-//   // }
+  // getEmail() {
+  //   return this.email;
+  // }
 
-//   // setEmail(email) {
-//   //   this.email = email;
-//   // }
+  // setEmail(email) {
+  //   this.email = email;
+  // }
 
-//   getPassword() {
-//     return this.password;
-//   }
+  getPassword() {
+    return this.password;
+  }
 
-//   setPassword(password) {
-//     this.password = password;
-//   }
-// }
+  setPassword(password) {
+    this.password = password;
+  }
+}
 
-// module.exports = User;
+module.exports = User;
